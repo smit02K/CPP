7bdb8a97a24d6038d021f8463dbec4709ac52ff3
smit02K <63186967+smit02K@users.noreply.github.com>
2020-09-10 23:39:23 +0530

    Add files via upload

---
+++ add1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int num1,num2,ans;
+
+   num1 = 5;
+    
+   num2 = 4;
+   
+   ans = num1 + num2;
+   
+   cout<<"sum of "<<num1<<" and "<<num2<<" is: "<<ans;;
+   return 0;	
+}
---
+++ print_num2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int num;
+
+   cout<<"enter number: ";
+   cin>>num;
+   
+   cout<<"you have entered : "<<num;
+   return 0;	
+}
